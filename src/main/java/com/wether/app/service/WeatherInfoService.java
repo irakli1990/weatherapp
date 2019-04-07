595a29b39a17987252ee064e4868b5651361ab53
Irakli <iraklikardawa@gmail.com>
2019-04-07 20:04:07 +0200

    Added function description

--- src/main/java/com/wether/app/service/WeatherInfoService.java
+++ src/main/java/com/wether/app/service/WeatherInfoService.java
@@ -27,11 +27,13 @@ public class WeatherInfoService {
     }
 
 
+    // URL AND APLICATION KEY
     private static final String STATIC_WEATHER_URL =
         "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=e7b429593963ceb11a198af8d22c9a55";
 
 
 
+    // METHOD FINDING ALL INFORMATION FROM OPENWEATHERMAP
     public WeatherInfo getWeather(String city) {
         Reader reader =
             parsUrltoJsonObject(String.format(STATIC_WEATHER_URL, city));
@@ -40,12 +42,14 @@ public class WeatherInfoService {
         return weatherInfo;
     }
 
+    // METHOD FINDING MAIN FROM OPENWEATHER MAP
     public Main getMain(String city){
         Main main = getWeather(city).getMain();
 
         return main;
     }
 
+    // METHOD READING READING STREAM FROM URL
     public Reader parsUrltoJsonObject(String url) {
         InputStream is;
         Reader reader = null;
